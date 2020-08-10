13d0bef084e07ddf72fb9abb1fa16a0c3583313b
Jorge Sevillano <jorgesemon@gmail.com>
2020-08-10 16:21:49 +0200

    Fix failing test

--- src/App.test.js
+++ src/App.test.js
@@ -2,8 +2,8 @@ import React from 'react';
 import { render } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders navbar', () => {
   const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
+  const linkElement = getByText(/Reinforcement learning/i);
   expect(linkElement).toBeInTheDocument();
 });
