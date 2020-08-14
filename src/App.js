c631c25674b775b9d1d009525aade9fe2ada95e5
Jorge Sevillano <jorgesemon@gmail.com>
2020-08-14 11:18:34 +0200

    Refactor

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
-import React from 'react';
-import Navbar from './Navbar/NavBar';
-import MainContainer from './MainContainer/MainContainer';
+import React from 'react'
+import Navbar from './Navbar/NavBar'
+import MainContainer from './MainContainer/MainContainer'
 import './App.css'
 
 function App() {
@@ -9,7 +9,7 @@ function App() {
       <Navbar/>
       <MainContainer/>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,9 @@
-import React from 'react';
-import { render } from '@testing-library/react';
-import App from './App';
+import React from 'react'
+import { render } from '@testing-library/react'
+import App from './App'
 
 test('renders navbar', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/Reinforcement learning/i);
-  expect(linkElement).toBeInTheDocument();
+  const { getByText } = render(<App />)
+  const linkElement = getByText(/Reinforcement learning/i)
+  expect(linkElement).toBeInTheDocument()
 });
